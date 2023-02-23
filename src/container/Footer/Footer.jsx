fbc160fb3493c5efb82ed76e93ac5196dd3ac3b3
Ashit Kumar Sinha <37330958+asitkr@users.noreply.github.com>
2023-02-23 18:59:19 +0530

    Add files via upload

---
+++ src/container/Footer/Footer.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
+
+import { FooterOverlay, Newsletter } from '../../components';
+import { images } from '../../constants';
+import './Footer.css';
+
+const Footer = () => (
+  <div className="app__footer section__padding" id="login">
+    <FooterOverlay />
+    <Newsletter />
+
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">21 harbour street Port Antonio, Portland, Jamaica</p>
+        <p className="p__opensans">+1 876-777-7287</p>
+        <p className="p__opensans">+1 876-895-9388</p>
+      </div>
+
+      <div className="app__footer-links_logo">
+        <h1>Ricky's</h1>
+        <p className="p__opensans">&quot;The best way to find yourself is to lose yourself in the service of others.&quot;</p>
+        <img src={images.spoon} className="spoon__img" style={{ marginTop: 15 }} />
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
+      </div>
+
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Working Hours</h1>
+        <p className="p__opensans">Monday-Friday:</p>
+        <p className="p__opensans">08:00 am - 12:00 am</p>
+        <p className="p__opensans">Saturday-Sunday:</p>
+        <p className="p__opensans">07:00 am - 11:00 pm</p>
+      </div>
+    </div>
+
+    <div className="footer__copyright">
+      <p className="p__opensans">2021 Ricky's. All Rights reserved.</p>
+    </div>
+
+  </div>
+);
+
+export default Footer;
